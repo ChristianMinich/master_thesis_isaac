59a58f4e60f385c44fbd3422dbb6bfe46eb08d93
Christian Minich <chris.minich.cm@gmail.com>
2026-07-08 17:57:57 +0900

    vald run

---
+++ isaac_unitree_starter/vald_run.py
@@ -0,0 +1,19 @@
+conda activate env_isaaclab
+
+python -u - <<'PY'
+from isaacsim.simulation_app import SimulationApp
+import time
+
+print("1 before app")
+app = SimulationApp({"headless": False})
+print("2 after app")
+
+for i in range(100):
+    app.update()
+
+print("3 before close")
+app.close()
+print("4 after close")
+PY
+
+echo "exit code: $?"
